f8e35854d423737446c7aa79b43b812ad56480f2
Weslley Cardoso <weslleycardoso011@gmail.com>
2026-05-08 18:18:47 -0300

    feat(mobile): implementar service de autenticação com TDD
    - Cria __tests__/services/auth.test.ts com 10 casos cobrindo login, obterToken, obterVeiculoId e limparSessao
    - Cria services/auth.ts com funções login, obterToken, obterVeiculoId e limparSessao
    - Remove jest.setup.ts vazio e sua referência no jest.config.js
    - Adiciona expo-modules-core ao transformIgnorePatterns do Jest

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   preset: 'jest-expo',
-  setupFiles: ['./jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-expo|expo|@expo|expo-router|expo-secure-store|react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!(jest-expo|expo|@expo|expo-router|expo-secure-store|expo-modules-core|react-native|@react-native|@react-navigation)/)',
   ],
   testMatch: [
     '**/__tests__/**/*.{ts,tsx}',
